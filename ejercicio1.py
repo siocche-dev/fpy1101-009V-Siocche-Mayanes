1ba76a4f64e582026dc2f280d11d7795f24f3887
siocche <siocche@gmail.com>
2026-06-06 18:36:59 -0400

    h

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,45 @@
+cantidad_medicos = 0
+especialista_senior = 0
+residente_junior = 0
 
+
+while True:
+    print("Bienvenido al sistema de registro de médicos, Hospital Central Metropolitano")
+    try:
+        cantidad_medicos = int(input("Ingrese la cantidad de médicos a registrar: "))
+        if cantidad_medicos <= 0:
+            print ("¡Registro médico inválido! Ingresa un entero positivo para continuar.")
+        else: 
+            print ("Se han registrado", cantidad_medicos, "médicos")
+            break
+    except ValueError:
+        print ("¡Registro médico inválido! Ingresa un entero positivo para continuar.")
+        
+for i in range (cantidad_medicos):
+    print ("\nIngresando el médico", (i+1))
+    
+    while True:
+        alias = input("Ingrese el nombre del profesional (min. 6 caracteres, sin espacio):").strip()
+        if (len(alias) <6) or (' ' in alias):
+            print ("El nombre del profesional debe tener al menos 6 caracteres y no tener espacios en blanco")
+        else:
+            break
+    
+    while True:
+        try:
+            experiencia = int(input("Ingrese su experiencia clínica en años: "))
+            if experiencia < 0:
+                print ("¡Error clínico! Ingresa un número entero positivo para la experiencia.")
+            else:
+                if experiencia >= 5:
+                    especialista_senior = especialista_senior + 1
+                    print ("El médico registrado es un especialista senior.")
+                else:
+                    residente_junior = residente_junior + 1
+                    print ("El médico registrado es un residente junior.")
+                break 
+        except ValueError:
+            print ("¡Error clínico! Ingresa un número entero positivo para la experiencia.")
+        
+print ("\nEl hospital cuenta con ", especialista_senior, "especialistas senior y", residente_junior, "residentes junior! ¡Sistema listo para operar!")
+    
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,62 @@
+stocklibros = 120
+cantidad_prestamos = 0
+prestamo_libros = 0
 
+
+print ("Bienvenido al sistema de gestión de préstamos de la Biblioteca Central")
+
+while True:
+    print("\n---Menú principal---")
+    print("1. Libros disponibles")
+    print("2. Realizar préstamo")
+    print("3. Devolver libro")
+    print("4. Historial de préstamos")
+    print("5. Salir")
+    try:
+        opcion = int(input("Seleccione una opción: "))
+    except ValueError:
+        print("¡Opción inválida! Por favor, ingresa una opción del menú.")
+        continue
+    
+    if opcion == 1:
+        print("Libros disponibles:", stocklibros)
+    elif opcion == 2:
+        try:
+            cantidad_prestamos = int(input("Ingrese la cantidad de libros a prestar: "))
+            if cantidad_prestamos <= 0:
+                print(" Cantidad inválida, ingrese un número entero")
+            elif cantidad_prestamos > stocklibros:
+                print("No debe superar el stock disponible")
+            else:
+            
+                prestamo_libros += cantidad_prestamos
+                stocklibros -= cantidad_prestamos
+                print ("Se han prestado", cantidad_prestamos, "libros.")
+        except ValueError:
+            print ("¡Cantidad inválida! Debe ingresar un número entero positivo.")
+            
+    elif opcion == 3:
+        try:
+            cantidad_devoluciones = int(input("Ingrese la cantidad de libros a devolver: "))
+            if cantidad_devoluciones <= 0:
+                print("Cantidad inválida, ingrese un número entero")
+            elif cantidad_devoluciones > prestamo_libros:
+                print("No debe superar la cantidad de libros prestados")
+            elif stocklibros + cantidad_devoluciones > 120:
+                print("No debe superar el stock de libros (120 libros)")
+            else:
+                stocklibros += cantidad_devoluciones
+                prestamo_libros -= cantidad_devoluciones
+                print("Se han devuelto", cantidad_devoluciones, "libros.")
+        except ValueError:
+            print("¡Cantidad inválida! Debe ingresar un número entero positivo.")
+    elif opcion == 4:
+        print("\n---Historial de préstamos---")
+        print("Libros prestados:", prestamo_libros)
+  
+    elif opcion == 5:
+        print("¡Gracias por utilizar nuestro software, hasta la próxima!")
+        break
+    else:
+        print ("Opción inválida. Por favor, seleccione una opción del menú.")
+        
